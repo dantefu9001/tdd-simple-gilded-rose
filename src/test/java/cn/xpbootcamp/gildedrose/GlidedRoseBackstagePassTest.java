283925739b90d1652add390bea86d10c29698c4e
zenan <zenan.fu@thoughtworks.com>
2020-08-23 20:18:08 +0800

    add test for base product and backstage pass

---
+++ src/test/java/cn/xpbootcamp/gildedrose/GlidedRoseBackstagePassTest.java
@@ -0,0 +1,26 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.Test;
+
+public class GlidedRoseBackstagePassTest {
+
+    @Test
+    public void should_increase_by_one_everyday_until_ten_days_before_due(){
+
+    }
+
+    @Test
+    public void should_increase_by_double_everyday_from_ten_days_to_five_days_before_due(){
+
+    }
+
+    @Test
+    public void should_increase_by_triple_everyday_five_days_before_due(){
+
+    }
+
+    @Test
+    public void should_become_0_immediately_after_due(){
+
+    }
+}
---
+++ src/test/java/cn/xpbootcamp/gildedrose/GlidedRoseNormalTest.java
@@ -0,0 +1,20 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.Test;
+
+public class GlidedRoseNormalTest {
+    @Test
+    public void should_the_value_between_0_and_50(){
+
+    }
+
+    @Test
+    public void should_the_quality_drop_by_one_everyday_before_due(){
+
+    }
+
+    @Test
+    public void should_the_quality_drop_by_double_everyday_after_due(){
+
+    }
+}
